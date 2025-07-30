f485caf27624a6df2025e559acadb9cf6d77fbba
BassilekinJean <jean.bassilekin@facsciences-uy1.cm>
2025-07-30 17:41:34 +0100

    Ajout de l'annotation @Repository dans l'interface UserRepository pour améliorer la clarté et la gestion des composants Spring.

--- src/main/java/com/bassilekin/report_generator/Repository/UserRepository.java
+++ src/main/java/com/bassilekin/report_generator/Repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.bassilekin.report_generator.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bassilekin.report_generator.Entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
     User findByUserEmail(String email);
